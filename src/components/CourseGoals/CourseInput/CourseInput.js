cb5490baac9a52d424ed5bad21ff54e803175301
revivesanjeev <revivesanjeev324@gmail.com>
2024-05-11 10:21:46 +0530

    Add goal button making blur

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+  const goalInputChangeHandler = (event) => {
+    const value = event.target.value.trim();
+    setEnteredValue(value);
+    setIsValid(value.length > 0);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (!enteredValue) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setEnteredValue(""); // Clear input after submitting
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            background: !isValid ? "salmon" : "white",
+          }}
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit" disabled={!isValid}>
+        Add Goal
+      </Button>
     </form>
   );
 };
 
 export default CourseInput;
+
